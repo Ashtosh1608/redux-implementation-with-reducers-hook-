6b2426d9e4f92e854eb3131643069334c0c177e2
Ashtosh Kumar <ashtosh16082001@gmail.com>
2023-12-31 17:06:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Task from "./Components/Task";
+import TaskList from "./Components/TaskList";
+// import "./index.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Task />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
